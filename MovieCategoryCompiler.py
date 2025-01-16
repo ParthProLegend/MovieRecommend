87554da9827146d46881cc06909c11026cfc3e06
Parth <99063996+ParthProLegend@users.noreply.github.com>
2025-01-16 13:27:55 +0530

    Main Program Files

---
+++ MovieCategoryCompiler.py
@@ -0,0 +1,34 @@
+
+OriginalFile = "Inside Lab\\WWC_24\\FinalProject\\OriginalMovieData\\title.basics.tsv"
+# File100 = "Inside Lab\\WWC_24\\FinalProject\\CleanMovieData\\Titles100.tsv"
+CompiledCategoryFile = "Inside Lab\\WWC_24\\FinalProject\\CompiledData\\CategoryData.txt"
+
+def MovieCategoryAggregator():
+    categories_liked = dict()
+    with open(CompiledCategoryFile, mode="r", encoding="utf-8") as CompiledMovieCategoryData:
+        temp = CompiledMovieCategoryData.read().strip()
+        if temp == "":
+            MovieCategoryAggregatorSubProcess()
+    with open(CompiledCategoryFile, mode="r", encoding="utf-8") as CompiledMovieCategoryData:
+        for EachLine in CompiledMovieCategoryData:
+            categories_liked[EachLine.strip()] = 0
+
+
+    return categories_liked
+
+
+def MovieCategoryAggregatorSubProcess():
+    categories = list()
+    with open(OriginalFile, mode="r", encoding="utf-8") as raw_data:
+        for EachLine in raw_data:
+            EachCategory = EachLine.split("\t")[-1].strip()
+            EachCategory = EachCategory.split(",")
+            for category in EachCategory:
+                if category not in categories:
+                    if category == "genres":
+                        continue
+                    categories.append(category.strip())
+    with open(CompiledCategoryFile, mode="a", encoding="utf-8") as CompiledMovieData:
+        for category in categories:
+            category = "Undefined".strip() if category == r"\N" else category.strip()
+            CompiledMovieData.write(category + "\n")
---
+++ MovieDataCleaner.py
@@ -0,0 +1,26 @@
+def DataCleanerService():
+    # j = 0
+
+    # OriginalFile = "Inside Lab\\WWC_24\\FinalProject\\OriginalMovieData\\title.basics.tsv"
+    # File100 = "Inside Lab\\WWC_24\\FinalProject\\CleanMovieData\\Titles100.tsv"
+    CompiledCategoryFile = "Inside Lab\\WWC_24\\FinalProject\\CompiledData\\CategoryData.txt"
+    MovieData = "Inside Lab\\WWC_24\\FinalProject\\CompiledData\\MovieData.txt"
+
+
+    # Clearing Old File Data or Creating New Files
+    with open(CompiledCategoryFile, mode="w", encoding="utf-8") as dump:
+        dump.write("")
+        pass
+    with open(MovieData, mode="w", encoding="utf-8") as dump:
+        dump.write("")
+        pass
+    # with open(File100, mode="w", encoding="utf-8") as dump:
+        # dump.write("")
+
+    # Creating New Files With New Data
+    # with open(OriginalFile, mode="r", encoding="utf-8") as data, open(File100, mode="a", encoding="utf-8") as newfiledata2:
+    #     for EachLine in data:
+    #         newfiledata2.write(EachLine)
+    #         j += 1
+    #         if j > 99:
+    #             break
